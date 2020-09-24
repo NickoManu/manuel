c6ad862415a3a4cb60f831c9af2a00be8aa648b5
NickoManu <71433814+NickoManu@users.noreply.github.com>
2020-09-24 12:33:48 +0200

    add esercizi2.1

---
+++ Esercitazioni2_1/Es_2_1_2/src/Luminosita.java
@@ -0,0 +1,8 @@
+
+public interface Luminosita {
+
+	public void setLuminosita(int l);
+	public void brighter();
+	public void darker();
+	
+}
---
+++ Esercitazioni2_1/Es_2_1_2/src/Riproducibile.java
@@ -0,0 +1,10 @@
+
+public interface Riproducibile {
+
+	public void setDurata(int d);
+	public void play();
+	public void setVolume(int v);
+	public void weaker();
+	public void louder();
+	
+}
